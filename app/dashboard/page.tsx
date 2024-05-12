5c3ab1503705e81447e63d9093f5c92433242385
Amirmohammad Fallah <amirmfallah@yahoo.com>
2024-05-12 19:01:54 +1000

    added chat ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { CornerDownLeft, Mic, Paperclip } from "lucide-react";
 
-interface Props {}
-
-const Page: React.FC<Props> = () => {
-  return <div>Hello world</div>;
-};
-
-export default Page;
+export default function Page() {
+  return (
+    <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+      <TooltipProvider>
+        <div className="space-y-6 max-h-[70vh] overflow-auto flex-1 pb-6 flex flex-col"></div>
+        <form
+          className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+          x-chunk="dashboard-03-chunk-1"
+        >
+          <Label htmlFor="message" className="sr-only">
+            Message
+          </Label>
+          <Textarea
+            id="message"
+            placeholder="Type your message here..."
+            className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+          />
+          <div className="flex items-center p-3 pt-0">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Paperclip className="size-4" />
+                  <span className="sr-only">Attach file</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="top">Attach File</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Mic className="size-4" />
+                  <span className="sr-only">Use Microphone</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="top">Use Microphone</TooltipContent>
+            </Tooltip>
+            <Button type="submit" size="sm" className="ml-auto gap-1.5">
+              Send Message
+              <CornerDownLeft className="size-3.5" />
+            </Button>
+          </div>
+        </form>
+      </TooltipProvider>
+    </div>
+  );
+}
